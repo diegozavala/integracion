8bb943b6303abfa517e9be47478445f246f84f0d
root <root@integra2.ing.puc.cl>
2014-05-06 14:37:19 -0400

    primera

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,86 @@
+class HomesController < ApplicationController
+  before_action :set_home, only: [:show, :edit, :update, :destroy]
+
+  # GET /homes
+  # GET /homes.json
+  def index
+    test_ftp
+    @homes = Home.all
+  end
+
+  # GET /homes/1
+  # GET /homes/1.json
+  def show
+  end
+
+  # GET /homes/new
+  def new
+    @home = Home.new
+  end
+
+  # GET /homes/1/edit
+  def edit
+  end
+
+  # POST /homes
+  # POST /homes.json
+  def create
+    @home = Home.new(home_params)
+
+    respond_to do |format|
+      if @home.save
+        format.html { redirect_to @home, notice: 'Home was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @home }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @home.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /homes/1
+  # PATCH/PUT /homes/1.json
+  def update
+    respond_to do |format|
+      if @home.update(home_params)
+        format.html { redirect_to @home, notice: 'Home was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @home.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /homes/1
+  # DELETE /homes/1.json
+  def destroy
+    @home.destroy
+    respond_to do |format|
+      format.html { redirect_to homes_url }
+      format.json { head :no_content }
+    end
+  end
+  
+  def test_ftp
+   
+    sftp1=Net::SFTP
+    sftp1.start('http://integra.ing.puc.cl',:port =>21,:user=> 'grupo2', :password => 'apijd9292') do |sftp|
+        @status=sftp.status
+    end
+    
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_home
+      @home = Home.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def home_params
+      params[:home]
+    end
+    
+    
+end
