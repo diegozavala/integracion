25268d893260d30171e98cd720c822094002efca
diegozavala <diegozavala@gmail.com>
2014-05-07 23:23:27 -0400

    ftp

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -62,13 +62,17 @@ class HomesController < ApplicationController
     end
   end
   
-  def test_ftp
-    require 'rubygems' 
+  def test_ftp 
     require 'net/sftp' 
    
     sftp2=Net::SFTP
-    sftp2.start('integra.ing.puc.cl',:port =>22,:user=> 'grupo2', :password => 'apijd9292') do |sftp|
-        @status=sftp.status
+    sftp2.start('integra.ing.puc.cl','grupo2', :password => 'apijd9292') do |sftp|
+      
+      cont=0
+      sftp.dir.foreach("/home/grupo2/Pedidos") do |entry|
+        cont+=1
+      end
+      @status=sftp.download!("/home/grupo2/Pedidos/pedido_10.xml")
     end
     
   end
