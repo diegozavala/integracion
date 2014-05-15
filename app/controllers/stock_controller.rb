a3346c2c4f9c735f83e3e69767d0a7b825b75258
cristobal jimenez <cijimenez90@gmail.com>
2014-05-15 11:25:31 -0400

    Gestion de stocks v1.2

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,16 +1,32 @@
 class StockController < ApplicationController
+	
 	def index
-
 	end
 
-	def test
-		request = RestClient.get 'http://example.com/resource', {:params => {:id => 50, 'foo' => 'bar'}}
+	#GET
+	def almacenes
+		api_request('almacenes', 'GET')
 	end
 
-	def generate_auth_hash
+	def skus_with_stock
+		api_request('skusWithStock', 'GET')
+	end
+
+	def sku
+		api_request('stock', 'GET')
+	end
+
+	def generate_auth_hash(action)
 		require 'openssl'
 		require "base64"
-		hash  = Base64.encode64(OpenSSL::HMAC.digest('sha1', STOCK_PRIVATE_KEY, "GET"))
-		auth = 'UC '+STOCK_PUBLIC_KEY+':'+hash
-	end
+		#funciona solo para test por ahora (por el GET)
+		#codigo basado en http://jokecamp.wordpress.com/2012/10/21/examples-of-creating-base64-hashes-using-hmac-sha256-in-different-languages/#ruby
+		hash  = Base64.encode64(OpenSSL::HMAC.digest('sha1', Integra2::STOCK_PRIVATE_KEY, action))
+		auth = 'UC '+Integra2::STOCK_PUBLIC_KEY+':'+hash
+		end
+
+	private
+		def api_request(url, action)
+			@request = JSON.parse(RestClient.get Integra2::STOCK_API_URL+url, {:Authorization => generate_auth_hash(action)})
+		end
 end
