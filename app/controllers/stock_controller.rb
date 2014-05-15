f2e72785bc804bca276bc243a9e82dbc85d17a4a
cristobal jimenez <cijimenez90@gmail.com>
2014-05-15 15:50:33 -0400

    agregue metodos que faltaban

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -3,7 +3,6 @@ class StockController < ApplicationController
 	def index
 	end
 
-	#GET
 	def almacenes
 		@request = JSON.parse(RestClient.get Integra2::STOCK_API_URL+'almacenes', {:Authorization => generate_auth_hash('GET')})
 	end
@@ -25,5 +24,29 @@ class StockController < ApplicationController
 		#codigo basado en http://jokecamp.wordpress.com/2012/10/21/examples-of-creating-base64-hashes-using-hmac-sha256-in-different-languages/#ruby
 		hash  = Base64.encode64(OpenSSL::HMAC.digest('sha1', Integra2::STOCK_PRIVATE_KEY, action))
 		auth = 'UC '+Integra2::STOCK_PUBLIC_KEY+':'+hash
-		end
+	end
+
+	#metodos de la api por usar
+	def move_stock
+		producto_id = ''
+		almacen_id = ''
+		@request = JSON.parse(RestClient.get Integra2::STOCK_API_URL+'moveStock', {:Authorization => generate_auth_hash('POST'+producto_id+almacen_id), :params=>{:almacenId=>almacen_id, :productId=>producto_id}})
+		#retorna Producto
+	end 
+
+	def move_stock_from_warehouse
+		producto_id = ''
+		almacen_id = ''
+		@request = JSON.parse(RestClient.get Integra2::STOCK_API_URL+'moveStockBodega', {:Authorization => generate_auth_hash('POST'+producto_id+almacen_id), :params=>{:almacenId=>almacen_id, :productId=>producto_id}})
+		#retorna Producto
+	end
+
+	def despachar
+		producto_id = ''
+		direccion = ''
+  precio=0
+		pedido_id =''
+		@request = JSON.parse(RestClient.get Integra2::STOCK_API_URL+'stock', {:Authorization => generate_auth_hash('DELETE'+producto_id+almacen_id), :params=>{:productId=>producto_id, :direccion=>direccion, :precio=>precio, :pedidoId=>pedido_id}})
+
+	end
 end
