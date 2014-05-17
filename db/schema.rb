31434ab7e699b69265aff620b164d9176739d4ea
administrator <administrator@integra2.ing.puc.cl>
2014-05-17 17:44:10 -0400

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -796,9 +796,4 @@ ActiveRecord::Schema.define(version: 20140517202943) do
     t.datetime "updated_at"
   end
 
-  create_table "stocks", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
