141e3d335260441c9a36f32346a37b855e1c80f8
cfontanet <fontanet@gmail.com>
2014-05-19 17:20:50 -0400

    Update api_controller.rb

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,14 +3,7 @@ class ApiController < ApplicationController
 protect_from_forgery with: null_session
 def pedir_productos
 
-user_name=params[:usuario]
-user_pass=params[:password]
-if ApiUser.where(:name => user_name).where(:password => user_pass).blank?
-	render :json => { :errors => "no funciona"}
-else
 render :json => { :errors => "funciona"}
-end
-
 
 
 end
