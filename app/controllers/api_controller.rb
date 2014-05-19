3097bde6951f59901178fe447432722eb8597381
toto <toto@ubuntu.(none)>
2014-05-19 18:40:09 -0400

    api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,8 +6,17 @@ def pedir_productos
 
 user_name=params[:usuario]
 user_pass=params[:password]
+bodega_destino = params[:almacen_id]
+sku = params[:SKU]
+cantidad = params[:cantidad]
+
 if ApiUser.where(:name => user_name).where(:password => user_pass).blank?
-	render :json => { :errors => user_name}
+	render :json => { :user_name => user_name,
+	:user_pass => user_pass,
+	:bodega_destino => bodega_destino,
+	:sku => sku,
+	:cantidad => cantidad
+	 }
 else
 	render :json => { :errors => "funciona"}
 end
