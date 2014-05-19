0c371e9f59f6028dfc7224dedc49146924d34d10
administrator <administrator@integra2.ing.puc.cl>
2014-05-19 17:06:24 -0400

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20140519195822) do
     t.datetime "updated_at"
   end
 
+  create_table "as", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "ftp_pedidos", force: true do |t|
     t.string   "nombre_archivo"
     t.integer  "numero_pedido"
