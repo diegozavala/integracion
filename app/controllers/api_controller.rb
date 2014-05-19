ec2e8f66f560127c1f225b59f16ce44f58e3294e
cfontanet <fontanet@gmail.com>
2014-05-19 17:28:40 -0400

    Update api_controller.rb

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,5 @@
 class ApiController < ApplicationController
-protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 def pedir_productos
 
 render :json => { :errors => "funciona"}
