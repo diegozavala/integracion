40f92b001701b0f1d6a371d050b1f542e9e8a368
toto <toto@ubuntu.(none)>
2014-06-15 17:56:03 -0400

    as

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1082,50 +1082,14 @@ class HomesController < ApplicationController
     end
 
 
-    #url_taxon = 'http://integra2.ing.puc.cl/store/api/taxonomies/8/taxons' 
-    # hash.each do |hash|
- 
-    #(HTTParty.post url_taxon, 
-    # :body => { 'taxon[name]' => hash[0], 'taxon[id]' => hash[1] ,'token' => "7771e9b7bd0676c2d5b4e2f424328b52a82add010ea9b1c2"})
-    #end 
+    url_taxon = 'http://integra2.ing.puc.cl/store/api/taxonomies/8/taxons' 
+    hash.each do |hash|
+     (HTTParty.post url_taxon, 
+     :body => { 'taxon[name]' => hash[0], 'taxon[id]' => hash[1] ,'token' => "7771e9b7bd0676c2d5b4e2f424328b52a82add010ea9b1c2"})
+    end 
 
 
-    a=0
-    require 'open-uri'
-    #Spree::Product.destroy_all
-    data.each do |data|
-      arr=[]
-      data['categorias'].each do |cat|
-        begin
-          arr<< hash[cat]+1947
-
-        rescue => e
-          a=a
-        end
-      end
-      open('public/imagenes/'+a.to_s+'.png', 'wb') do |file|
-        file << open(data['imagen']).read
-      end
-      product = Spree::Product.create(
-
-      :name => (data['marca']+" / "+ data['modelo']).to_s,
-      :price =>  data['precio']['internet'],
-      :shipping_category_id =>1,
-      :description => data['descripcion'],
-      :sku => data['sku'],
-      :available_on => Time.now,
-
-                                
-      # :taxon_ids => arr
-      )
-
-
-      prod = Spree::Product.last
-      prod.images << Spree::Image.create!(:attachment => open('public/imagenes/'+a.to_s+'.png')
-      )
-      a=a+1
-
-    end
+    
 
  
 
