1aa424040dda48cd4125cea42716387c9f8e1743
toto <toto@ubuntu.(none)>
2014-06-15 19:01:40 -0400

    a

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1091,47 +1091,7 @@ class HomesController < ApplicationController
 
     
 
-    a=0
-    require 'open-uri'
-    #Spree::Product.destroy_all
-    data.each do |data|
-      arr=[]
-      data['categorias'].each do |cat|
-        begin
-          arr<< hash[cat]+2
-
-        rescue => e
-          a=a
-        end
-      end
-      open('public/imagenes/'+a.to_s+'.png', 'wb') do |file|
-        file << open(data['imagen']).read
-      end
-      product = Spree::Product.create!(
-
-      :name => (data['marca']+" / "+ data['modelo']).to_s,
-      :price =>  data['precio']['internet'],
-      :shipping_category_id =>1,
-      :description => data['descripcion'],
-      :sku => data['sku'],
-      :available_on => Time.now,
-      :taxon_ids => arr
-      )
-      # Add current stock level
-      api_products = JSON.parse(get_stock(Integra2::ALMACEN_OTRO,data['sku'], 200))
-      product.on_hand = api_products.length
-      
-      product.save
-
-      prod = Spree::Product.last
-      prod.images << Spree::Image.create!(:attachment => open('public/imagenes/'+a.to_s+'.png')
-      )
-      a=a+1
-
-    end
-
- 
-
+    
 
 
 
