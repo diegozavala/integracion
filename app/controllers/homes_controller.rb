432180a05cf06e012979720a8afb9ff8dcb9bc70
toto <toto@ubuntu.(none)>
2014-06-16 00:03:53 -0400

    te volviste loca y disparaste frente a mi

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1119,7 +1119,7 @@ class HomesController < ApplicationController
       )
       # Add current stock level
       api_products = JSON.parse(get_stock(Integra2::ALMACEN_OTRO,data['sku'], 200))
-      product.on_hand
+      #product.on_hand
       
       product.save
 
