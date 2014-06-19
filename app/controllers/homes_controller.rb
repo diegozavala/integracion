f5cbcd6f8f94d40c8220aa566aad2cc2dad860cb
toto <toto@ubuntu.(none)>
2014-06-19 11:41:59 -0400

    bla

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1126,7 +1126,7 @@ class HomesController < ApplicationController
       # Add current stock level
       api_products = JSON.parse(get_stock(Integra2::ALMACEN_OTRO,data['sku'], 200))
       
-      s=Spree::StockItem.find_by_variant_id(product.id)
+      s=Spree::StockItem.find_by_variant_id(product.master.id)
       @info=api_products.count
       s.adjust_count_on_hand(3)
       
