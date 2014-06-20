445f8ae8dc93be9d03cfffe3eaf87218c148b4d2
toto <toto@ubuntu.(none)>
2014-06-19 23:20:45 -0400

    prueba

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,7 +1,16 @@
 Spree::Order.class_eval do
   checkout_flow do
-    gxfcv7fcjh khkj..hjoihiyv jl.khbnlhjfcvf
-    redirect_to :controller => "homes",:action => "index" 
+    go_to_state :complete
+  end
+
+  # If true, causes the payment step to happen during the checkout process
+  def payment_required?
+    return false
+  end
+
+  # If true, causes the confirmation step to happen during the checkout process
+  def confirmation_required?
+    return true
   end
 
 end
