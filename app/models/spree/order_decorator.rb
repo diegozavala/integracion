642212c474ebcf2e7c4342ec0e5b90a36f894ca3
toto <toto@ubuntu.(none)>
2014-06-20 00:35:06 -0400

    prueba

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -27,7 +27,7 @@ Spree::Order.class_eval do
   ## ACA TOY HAY Q METER EL DESCUENTO DE STOCK   #
   ##                                             #
   ################################################
-  products=Spree::Order.last.products
+
 
   end
   alias_method_chain :finalize!, :discount_stock
