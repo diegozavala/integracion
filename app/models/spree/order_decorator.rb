968911cad8bc7b9a600635f4a733f8bf1b40e8e4
toto <toto@ubuntu.(none)>
2014-06-20 00:29:49 -0400

    prueba

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -29,9 +29,7 @@ Spree::Order.class_eval do
   ################################################
   products=Spree::Order.last.products
 
-  products.each do |prod|
-    sku=prod.master.sku
-  end
+  
 
   end
   alias_method_chain :finalize!, :discount_stock
