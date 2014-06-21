3f53725d4ee0c168345ca494bb1291c20e2e05b1
toto <toto@ubuntu.(none)>
2014-06-21 15:36:48 -0400

    arreglo finalize y creacion orders

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,9 +4,24 @@ Spree::Order.class_eval do
    # go_to_state :payment, :if => lambda { |order| order.payment_required? }
     go_to_state :confirm
     go_to_state :complete
+
+
+
+
+
   end
 
 
+def finalize_with_drop_ship!
+    finalize_without_drop_ship!
+     order = Spree::Order.last
+    address = Spree::Address.find(order.bill_address_id)
+    products = order.products
+    products.each do |product|
+      despachar_stock(product.id, address.address1, product.price, order.number)
+    end
+  end
+  alias_method_chain :finalize!, :drop_ship
   
 
 end
