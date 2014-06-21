b0675be888fb027ab6f1e3b8654e1dc7afa931ac
toto <toto@ubuntu.(none)>
2014-06-21 14:31:32 -0400

    arreglo finalize y creacion orders

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -2,33 +2,9 @@ Spree::Order.class_eval do
   checkout_flow do
     go_to_state :address
    # go_to_state :payment, :if => lambda { |order| order.payment_required? }
-   #go_to_state :confirm
+    go_to_state :confirm
     go_to_state :complete
-
-
-
-
-
   end
-
-  # If true, causes the payment step to happen during the checkout process
-  def payment_required?
-    return false
-  end
-
-
-def finalize_with_discount_stock!
-    finalize_without_discount_stock!
-
-    order = Spree::Order.last
-    address = Spree::Address.find(order.bill_address_id)
-    products = order.products
-    products.each do |product|
-      despachar_stock(product.id, address.address1, product.price, order.number)
-    end
-    
-  end
-  alias_method_chain :finalize!, :discount_stock
 
 
   
