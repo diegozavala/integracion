11bb038dd7e2c6d9c75ea6a8973cafee98fb6e27
toto <toto@ubuntu.(none)>
2014-06-21 14:10:43 -0400

    arreglo finalize y creacion orders

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -31,7 +31,7 @@ Spree::Order.class_eval do
     address = Spree::Address.find(order.bill_address_id)
     products = order.products
     products.each do |product|
-      despachar_stock(product.id, address, product.price, order.number)
+      despachar_stock(product.id, address.address1, product.price, order.number)
     end
   end 
   
