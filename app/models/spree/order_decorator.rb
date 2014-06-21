76b2124f0d98f458958757d837f10baa8b3fbfb9
toto <toto@ubuntu.(none)>
2014-06-21 16:06:06 -0400

    arreglo finalize y creacion orders

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,8 +1,8 @@
 Spree::Order.class_eval do
   checkout_flow do
-    go_to_state :address
-   # go_to_state :payment, :if => lambda { |order| order.payment_required? }
-    go_to_state :confirm
+    #go_to_state :address
+    #go_to_state :payment, :if => lambda { |order| order.payment_required? }
+    #go_to_state :confirm
     go_to_state :complete
 
 
