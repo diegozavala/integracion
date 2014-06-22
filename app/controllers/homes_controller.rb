2caef466b73c1a733ab84e61568b9e6184472c0d
diegozavala <diegozavala@gmail.com>
2014-06-22 17:57:08 -0400

    arreglos

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1417,7 +1417,7 @@ class HomesController < ApplicationController
 
     #bajar stock de spree
     p=Spree::Variant.where(sku: sku)
-    s=Spree::StockItem.find_by_variant_id(p.master.id)
+    s=Spree::StockItem.find_by_variant_id(p.id)
     s.adjust_count_on_hand(0-cantidad)
 
   end
